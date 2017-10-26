d4586d061f57781c4e8e1c177d6f01c4cfa2aa51
Kovacs Attila <attila.kovacs92@gmail.com>
2017-10-26 13:32:42 +0200

    megjegyzesek gomb

--- resources/views/layouts/products.blade.php
+++ resources/views/layouts/products.blade.php
@@ -5,9 +5,23 @@
     <!-- Section Title Row Start -->
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="section-title">
+            <div class="section-title" style="margin-bottom: 15px;">
                 <h3>Termékek & Szolgáltatások</h3>
-                <p>Vasáruinkat e-mailben vagy telefonon tudja megrendelni, mely telephelyünkön személyesen átvehető illetve igény szerint 30 km-es körzetben térítés ellenében kiszállítjuk.</p>
+                <div class="header-text">
+                <a type="button" role="button" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">Megjegyzések</a>
+                </div>
+
+            </div>
+            <div class="collapse" id="collapseExample">
+                <div class="well">
+                    <h4>Füstcső</h4>
+                    <p>Az általunk gyártott vastagfalú acél füstcsöveket különböző  szabványméretekben 250 mm-1000mm hosszúságban, illteve könyökidomainkat 45° és 90° fokos változatban 2 féle (szürke, fekete) színben  tudjuk kínálni. A füstcsövek falvastagsága 1,5 mm-2,0mm között változhat. Füstcsöveink megfelelnek az Uniós szabványoknak.</p>
+                    <hr>
+                    <h4>Vasudvar</h4>
+                    <p>Vasáruinkat e-mailben vagy telefonon (korosszerkezt@gmail.com, 0630/303-7097) tudja megrendelni, mely telephelyünkön személyesen átvehető illetve igény szerint 30 km-es körzetben térítés ellenében kiszállítjuk.</p> <hr>
+                    <h4>Szolgáltatásaink</h4>
+                    <p>Cégünk több éves tapasztalat birtokában széleskörű szolgáltatást nyújt a lemezmegmunkálás terén. Különböző egyedi gyártású gépek gyártását, gyártmánytervezését kínáljuk meglévő és új ügyfeleink számára a legkorszerűbb technológiák használatával.</p>
+                </div>
             </div>
         </div>
     </div> <!-- End of Section Title Row -->
@@ -17,7 +31,7 @@
             <!-- Portfolio Filter Start -->
             <div class="portfolio-filter text-center">
                 <ul class="nav">
-                    <li class="active" data-filter=".fustcso">Füstcső</li>
+                    <li class="active" data-filter=".fustcso" >Füstcső</li>
                     <li data-filter=".other">Egyéb termékek</li>
                     <li data-filter=".vasudvar">Vasudvar</li>
                     <li data-filter=".services">Szolgáltatásaink</li>
