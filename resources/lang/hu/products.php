d38568460d8a96dddf3199d735b9ba251a80886b
Tony <tony@Tonys-MacBook-Pro.local>
2018-06-06 16:14:56 +0200

    change about and iron text

--- resources/lang/hu/products.php
+++ resources/lang/hu/products.php
@@ -6,7 +6,7 @@ return [
     'smoke-text'    => 'Az általunk gyártott vastagfalú acél füstcsöveket különböző szabványméretekben 250 mm-1000mm hosszúságban, illteve könyökidomainkat 45° és 90° fokos változatban 2 féle (szürke, fekete) színben tudjuk kínálni. A füstcsövek falvastagsága 1,5 mm-2,0mm között változhat. Füstcsöveink megfelelnek az Uniós szabványoknak.',
     'other'         => 'EGYÉB TERMÉKEK',
     'iron'          => 'VASUDVAR',
-    'iron-text'     => 'Vasáruinkat e-mailben vagy telefonon (korosszerkezt@gmail.com, 0630/303-7097) tudja megrendelni, mely telephelyünkön személyesen átvehető illetve igény szerint 30 km-es körzetben térítés ellenében kiszállítjuk.',
+    'iron-text'     => 'Széles alapanyag raktárkészletünk nyitott- és zárt profilok, hideg- és melegen hengerelt táblalemezek rozsdamentes-, saválló-, szénacél lemezek nagy méretválasztékban állnak vevőink rendelkezésére. Vasáruinkat e-mailben vagy telefonon (korosszerkezt@gmail.com, 0630/303-7097) tudja megrendelni, mely telephelyünkön személyesen átvehető illetve igény szerint 30 km-es körzetben térítés ellenében kiszállítjuk.',
     'services'      => 'SZOLGÁLTATÁSAINK',
     'services-text' => 'Cégünk több éves tapasztalat birtokában széleskörű szolgáltatást nyújt a lemezmegmunkálás terén. Különböző egyedi gyártású gépek gyártását, gyártmánytervezését kínáljuk meglévő és új ügyfeleink számára a legkorszerűbb technológiák használatával.'
 ];
